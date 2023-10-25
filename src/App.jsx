6a584bf45a15e8f21711a477d0d065c588f33185
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2023-10-24 22:31:32 -0400

    lecture_42_srcImage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+// src image this way
+import reactImg from "./assets/react-core-concepts.png";
+
 function App() {
   return (
     <div>
       <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        {/* Don't src image this way */}
+        <img src={reactImg} alt="Stylized atom" />
+        {/* <img src="src/assets/react-core-concepts.png" alt="Stylized atom" /> */}
         <h1>React Essentials</h1>
         <p>
           Fundamental React concepts you will need for almost any app you are
