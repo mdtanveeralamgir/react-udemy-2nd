71bf3edcab809a3b3cf5e0466037a721525b41ee
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2023-10-29 01:19:20 -0400

    lecture_74_changeStateOnPrevVal

--- src/components/player/Player.jsx
+++ src/components/player/Player.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const handleIsEditing = () => {
+    setIsEditing((prev) => !prev);
+  };
+
+  let playerName = <span className="player-name">{name}</span>;
+  if (isEditing) playerName = <input type="text" required />;
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleIsEditing}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
