de4843ad5f0caed67a475f4777e11dce9b6e573e
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2023-11-24 01:31:22 +0600

    lecture_89_moveBoardtoApp

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import Player from "./components/player/Player";
 import GameBoard from "./components/gameBoard/GameBoard";
 import { useState } from "react";
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./winningCombination";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = "X";
@@ -13,7 +20,12 @@ function deriveActivePlayer(gameTurns) {
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
   const currentPlayer = deriveActivePlayer(gameTurns);
-
+  let gameBoard = initialGameBoard;
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+    gameBoard[row][col] = player;
+  }
   function handleSelectSquare(rowIndex, colIndex) {
     setGameTurns((prevTurn) => {
       const currentPlayer = deriveActivePlayer(prevTurn);
@@ -31,7 +43,7 @@ function App() {
           <Player name="Player 1" symbol="X" isActive={currentPlayer === "X"} />
           <Player name="Player 2" symbol="O" isActive={currentPlayer === "O"} />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
     </main>
