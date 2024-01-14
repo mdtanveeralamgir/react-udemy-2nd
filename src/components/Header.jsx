ae6fbbc973e6bc8f6a02d0bdae01a4bce0dfb1c7
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2024-01-13 23:33:33 -0500

    lecture_111_cssModule: Have a single css file applied to only one component/file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,12 @@
 import logo from "../assets/logo.png";
-import "./Header.css";
+import classes from "./Header.module.css";
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p style={{
-          color: 'red',
-          // 'text-align': 'left'
-          textAlign: 'left'
-      }}>A community of artists and art-lovers.</p>
+      <p>A community of artists and art-lovers.</p>
     </header>
   );
 }
